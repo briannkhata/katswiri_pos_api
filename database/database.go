db67823bdac0084b2d1d5d4243b2614160346691
Brian Nkhata <bnkhata@mra.mw>
2025-01-10 10:16:49 +0200

    WIP

--- database/database.go
+++ database/database.go
@@ -1,33 +1,9 @@
-// package database
-
-// import (
-// 	"log"
-
-// 	"github.com/jinzhu/gorm"
-// 	_ "github.com/jinzhu/gorm/dialects/mysql"
-// )
-
-// var (
-// 	DBConn *gorm.DB
-// )
-
-// func InitDB() {
-// 	var err error
-// 	dsn := "root@tcp(localhost:3306)/hokala?charset=utf8mb4&parseTime=True&loc=Local"
-// 	DBConn, err = gorm.Open("mysql", dsn)
-
-// 	if err != nil {
-// 		log.Fatalf("Failed to connect to MySQL database: %v", err)
-// 	}
-
-// 	DBConn.LogMode(true)
-// 	log.Println("Connected to MySQL database successfully.")
-// }
-
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -39,8 +15,13 @@ var (
 
 func InitDB() {
 	var err error
-	dsn := "root@tcp(localhost:3306)/hokala?charset=utf8mb4&parseTime=True&loc=Local"
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 	DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to MySQL database: %v", err)
